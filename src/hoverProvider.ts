23802c31ba147354a74b8835fd5d6ae7e5a97417
halfrim0-0 <hitonihiki.develop@gmail.com>
2023-12-15 14:05:55 +0900

    feat: 設定で表示先が変わるように実装

--- src/hoverProvider.ts
+++ src/hoverProvider.ts
@@ -21,11 +21,22 @@ export class HoverProvider {
     this.context.globalState.update("text", currentWord);
 
     // ホバー時に表示する内容
-    const commandUri = vscode.Uri.parse("command:showAnnotation");
-    const contents = new vscode.MarkdownString(
-      `[アノテーションを表示](${commandUri})`
-    );
-    contents.isTrusted = true;
-    return Promise.resolve(new vscode.Hover(contents));
+    const config = vscode.workspace.getConfiguration("displayType");
+    const displayInTheBrowser = config.get<Boolean>("displayInTheBrowser");
+    if (displayInTheBrowser) {
+      const uri = `http://127.0.0.1:5000/annotation/show?text=${currentWord}&type=browser`;
+      const contents = new vscode.MarkdownString(
+        `[アノテーションを表示](${uri})`
+      );
+      contents.isTrusted = true;
+      return Promise.resolve(new vscode.Hover(contents));
+    } else {
+      const commandUri = vscode.Uri.parse("command:showAnnotation");
+      const contents = new vscode.MarkdownString(
+        `[アノテーションを表示](${commandUri})`
+      );
+      contents.isTrusted = true;
+      return Promise.resolve(new vscode.Hover(contents));
+    }
   }
 }
